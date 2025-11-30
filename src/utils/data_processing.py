cec73b709e468aafcd89a0282383c1d7eecf6dcd
Amira <ammou35@ulaval.ca>
2025-11-30 14:31:47 -0500

    refactor : new database

--- src/utils/data_processing.py
+++ src/utils/data_processing.py
@@ -4,33 +4,21 @@ from pathlib import Path
 
 # Charger les données relatifs aux films à partir du fichier CSV
 def load_movies(csv_path):
-    df = pd.read_csv(csv_path, na_values=["", " "])
+    df = pd.read_csv(csv_path, na_values=["", " "], quotechar='"')
 
-    # Transformation des colonnes nécessaires
-    df["release_date"] = pd.to_datetime(df["release_date"], errors="coerce")
-    df["release_year"] = df["release_date"].dt.year
-    df["adult"] = df["adult"].astype(str).str.lower() == "true"
-    df["adult"] = df["adult"].astype(int)
+    # Transformation de la date de sortie en année de sortie
+    df["date_x"] = df["date_x"].astype(str).str.strip()
+    df["date_x"] = pd.to_datetime(df["date_x"], format="%m/%d/%Y", errors="coerce")
+    idx = df.columns.get_loc("date_x")
+    df.insert(idx, "year", df["date_x"].dt.year)
 
-    # Colonnes à supprimer
-    df = df.drop(columns=["status", "release_date", "backdrop_path", "homepage", "imdb_id", "popularity", "poster_path"], errors="ignore")
+    # Colonnes à supprimer car jamais traitées
+    df = df.drop(columns=["date_x", "status", "orig_title"], errors="ignore")
 
     # Remplacer les valeurs 0 par NaN pour les colonnes avec des valeurs aberrantes
-    for feature in ["vote_average", "vote_count", "runtime", "budget", "revenue"]:
+    for feature in ["score", "revenue"]:
         df[feature] = pd.to_numeric(df[feature], errors="coerce")
         df.loc[df[feature] == 0, feature] = np.nan
-
-    # Filtrer les films où il manque trop d'informations
-    required = ["vote_average", "vote_count", "revenue", "runtime", "budget", "release_year"]
-    df = df.dropna(subset=required, how="all")
-    df = df.dropna(subset=["title"])
-    
-    # Filtrer les films avec le même id en gardant celui avec le moins de valeurs manquantes
-    missing_count = df.isna().sum(axis=1)
-    df = df.loc[missing_count.sort_values().index].drop_duplicates(subset="id", keep="first")
-
-    # Remplacer les NaN des caractéristiques pertinentes par des moyennes
-    for feature in required:
         mean_val = df[feature].mean(skipna=True)
         df[feature] = df[feature].fillna(mean_val)
 
@@ -42,15 +30,14 @@ def load_movies(csv_path):
 
 def load_for_clustering():
     df = pd.read_parquet("data/processed/clean_data.parquet")
-    feature_cols = ["vote_average", "vote_count", "revenue", "runtime", "adult", "budget", "release_year"]
+    feature_cols = ["year", "score", "budget_x", "revenue"]
     df = df[feature_cols]
     df.to_parquet("data/processed/clustering_data.parquet", index=False)
 
 
 def load_for_embed():
     df = pd.read_parquet("data/processed/clean_data.parquet")
-    feature_cols = ["title", "original_language", "original_title", "overview", "tagline", "genres", "production_companies",
-                    "production_countries", "spoken_languages", "keywords"]
+    feature_cols = ["names", "genre", "overview", "crew", "orig_lang", "country"]
     df = df[feature_cols]
     df.to_parquet("data/processed/embed_data.parquet", index=False)
 
@@ -62,6 +49,6 @@ def count_nans(df):
     return nan_counts
 
 
-load_movies("data/raw/TMDB_movie_dataset_v11_25-11-2025_9_32.csv")
+load_movies("data/raw/imdb_movies.csv")
 load_for_clustering()
 load_for_embed()
