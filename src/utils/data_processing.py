63db305c23927231a706a06c5e78c28e351d403a
Amira <ammou35@ulaval.ca>
2025-11-30 01:36:57 -0500

    feat: add parquet datasets for each model

--- src/utils/data_processing.py
+++ src/utils/data_processing.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from pathlib import Path
 
 # Charger les données relatifs aux films à partir du fichier CSV
 def load_movies(csv_path):
@@ -23,21 +24,35 @@ def load_movies(csv_path):
     required = ["vote_average", "vote_count", "revenue", "runtime", "budget", "release_year"]
     df = df.dropna(subset=required, how="all")
     df = df.dropna(subset=["title"])
+    
+    # Filtrer les films avec le même id en gardant celui avec le moins de valeurs manquantes
+    missing_count = df.isna().sum(axis=1)
+    df = df.loc[missing_count.sort_values().index].drop_duplicates(subset="id", keep="first")
 
     # Remplacer les NaN des caractéristiques pertinentes par des moyennes
     for feature in required:
         mean_val = df[feature].mean(skipna=True)
         df[feature] = df[feature].fillna(mean_val)
-    
-    # Remplacer les NaN de la colonne "adult" par True (just in case !)
-    df["adult"] = df[feature].fillna(True).astype(bool)
-            
-    # Filtrer les films avec le même id en gardant celui avec le moins de valeurs manquantes
-    missing_count = df.isna().sum(axis=1)
-    df = df.loc[missing_count.sort_values().index].drop_duplicates(subset="id", keep="first")
 
-    X = df.to_numpy()
-    return X
+    # Sauvegarder le DataFrame nettoyé au format .parquet
+    base_parquet_path = Path("data/processed/clean_data.parquet")
+    base_parquet_path.parent.mkdir(parents=True, exist_ok=True)
+    df.to_parquet(base_parquet_path, index=False)
+
+
+def load_for_clustering():
+    df = pd.read_parquet("data/processed/clean_data.parquet")
+    feature_cols = ["vote_average", "vote_count", "revenue", "runtime", "adult", "budget", "release_year"]
+    df = df[feature_cols]
+    df.to_parquet("data/processed/clustering_data.parquet", index=False)
+
+
+def load_for_embed():
+    df = pd.read_parquet("data/processed/clean_data.parquet")
+    feature_cols = ["title", "original_language", "original_title", "overview", "tagline", "genres", "production_companies",
+                    "production_countries", "spoken_languages", "keywords"]
+    df = df[feature_cols]
+    df.to_parquet("data/processed/embed_data.parquet", index=False)
 
 
 def count_nans(df):
@@ -47,4 +62,6 @@ def count_nans(df):
     return nan_counts
 
 
-X = load_movies("data/raw/TMDB_movie_dataset_v11_25-11-2025_9_32.csv")
+load_movies("data/raw/TMDB_movie_dataset_v11_25-11-2025_9_32.csv")
+load_for_clustering()
+load_for_embed()
